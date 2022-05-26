dcc38a502e20e6a165e599b8f70e9d0c036444eb
AlanBackrie <alanboboran@gmail.com>
2022-05-26 10:43:00 +0700

    test widget

---
+++ lib/models/beol.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class BeolPage extends StatelessWidget {
+  const BeolPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Text('data')
+      ],
+    );
+  }
+}
